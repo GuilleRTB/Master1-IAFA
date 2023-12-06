6d61d90335454ce0a96c8585fdecfcc2df787aab
GuilleRTB <guilhem.ratabouil@gmail.com>
2023-12-06 17:54:59 +0100

    update exo2

--- AlgoAvance/metaHeuristique/traveling/exo2.py
+++ AlgoAvance/metaHeuristique/traveling/exo2.py
@@ -2,6 +2,11 @@ import sys
 import random
 import numpy as np
 import math
+import time
+
+MAX_depl = 100
+MAX_essais = 10
+
 
 # Question 2.1
 def initial_solution(villes) :
@@ -11,8 +16,8 @@ def initial_solution(villes) :
 
 # Question 2.2
 def compute_dist(ville1, ville2):
-    v1 = np.array((ville1.x,ville1.y))
-    v2 = np.array((ville2.x,ville2.y))
+    v1 = np.array((ville1[1],ville1[2]))
+    v2 = np.array((ville2[1],ville2[2]))
     x1, y1 = v1
     x2, y2 = v2
     distance = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
@@ -20,7 +25,7 @@ def compute_dist(ville1, ville2):
     return distance
 
 def calcul_dist_total(villes, taille):
-    point_dep = Ville(0,0,0)
+    point_dep = [0,0,0]
     villedep = villes[0]
     som_dist = compute_dist(point_dep,villedep)
 
@@ -60,5 +65,55 @@ def meilleur_voisin(villes, taille) :
 
 
 # Question 2.4
-def steepest_hill_climbing(villes, taille) :
-    
+def stetepest_hill_climbing(villes, taille):
+    calcul_villes = calcul_dist_total(villes, taille)
+    deplacement=0
+
+    for i in range(MAX_depl):
+        calcul_villes_v,villes_v = meilleur_voisin(villes, taille)
+        if calcul_villes_v < calcul_villes:
+            calcul_villes = calcul_villes_v
+            villes = villes_v
+            deplacement+=1
+        else:
+            return calcul_villes,villes,deplacement
+
+    return calcul_villes,villes,deplacement
+
+
+
+
+################################
+#####        MAIN          #####
+################################
+
+start = time.time()
+
+args = sys.argv[1:]
+if args != []:
+    nom_fichier = args[0]
+else:
+    print("Command Error: Usage: program.py <nom_fichier>")
+
+
+with open(nom_fichier,'r') as f:
+    i = 0
+
+    taille = int(f.readline())
+    villes = []
+    ville = [0,0,0]
+
+    for line in f:
+        for val in line.split():
+            if i == 0:
+                ville[0] = int(val)
+            if i == 1:
+                ville[1] = int(val)
+            if i == 2:
+                ville[2] = int(val)
+            i+=1
+        i=0
+        #print("--",id,"--",x,"--",y,"\n")
+        villes.append(ville.copy())
+    #print(villes)
+    print(stetepest_hill_climbing(villes, taille))
